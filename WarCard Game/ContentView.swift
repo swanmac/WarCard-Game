e50d26ede76a29f6481641aa08df38f0c6d1124c
Greg Swan <gmswan99@gmail.com>
2025-09-19 17:09:52 -0400

    Initial Commit

---
+++ WarCard Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarCard Game
+//
+//  Created by Greg Swan on 9/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarCard Game/WarCard_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCard_GameApp.swift
+//  WarCard Game
+//
+//  Created by Greg Swan on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WarCard_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
